a94ec9c99156aea3eeb39c9079f39cc8a95b9ff0
dejvis06 <dejvis.beqiraj@fullscreendigital.ro>
2022-10-11 11:46:22 +0200

    return access/refresh tokens in a json object

--- oauth2-client/src/main/java/example/OAuth2LoginController.java
+++ oauth2-client/src/main/java/example/OAuth2LoginController.java
@@ -16,29 +16,35 @@
 
 package example;
 
+import net.minidev.json.JSONObject;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Index controller.
  *
  * @author Rob Winch
  */
-@Controller
+@RestController
 public class OAuth2LoginController {
 
-	@GetMapping("/")
-	public String index(Model model, @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
-			@AuthenticationPrincipal OAuth2User oauth2User) {
-		model.addAttribute("userName", oauth2User.getName());
-		model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
-		model.addAttribute("userAttributes", oauth2User.getAttributes());
-		return "index";
-	}
+    @GetMapping("/")
+    public String index(Model model, @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+                        @AuthenticationPrincipal OAuth2User oauth2User) {
+       /* model.addAttribute("userName", oauth2User.getName());
+        model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
+        model.addAttribute("userAttributes", oauth2User.getAttributes());*/
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.appendField("accessToken", authorizedClient.getAccessToken().getTokenValue());
+        jsonObject.appendField("refreshToken", authorizedClient.getRefreshToken().getTokenValue());
+
+        return jsonObject.toString();
+    }
 
 }
